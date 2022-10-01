b853199a55b14a10091b5f52f8b435745609b105
aldencheung2 <aldencheung2@gmail.com>
2022-10-01 19:21:17 -0400

    Added start and end times

--- pullData.js
+++ pullData.js
@@ -81,10 +81,19 @@ async function analyzeEach(myJson){
     // console.log(data);
     return data;
 }
-
-async function getTweets (term, num) {
+// startTime and endTime is in ISO 8601
+// startTime is the oldest tweet, endTime is most recent
+// TODO set start and end time for tweets
+async function getTweets (term, num, startTime, endTime) {
     data = {};
-    const response = await fetch('https://api.twitter.com/2/tweets/search/recent?query=%22' + term + '%22%20-is%3Aretweet&max_results=' + num + '&tweet.fields=created_at,public_metrics&expansions=entities.mentions.username',
+    let apiString = 'https://api.twitter.com/2/tweets/search/recent?query=%22' + term + '%22%20-is%3Aretweet&max_results=' + num + '&tweet.fields=created_at,public_metrics&expansions=entities.mentions.username&sort_order=relevancy';
+    if(startTime != undefined){
+        apiString += '&start_time=' + startTime;
+    }
+    if(endTime != undefined){
+        apiString += '&end_time=' + endTime;
+    }
+    const response = await fetch(apiString,
     {
         headers:{
             "Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAEHMhgEAAAAABGmQmPZGpdlv5MXacZeb%2BmAQQXw%3DGnhVwzX7PaLUXMRmZt6sWDjwrHUUnBGvQhQhvAkOOsos9VqD1n"
