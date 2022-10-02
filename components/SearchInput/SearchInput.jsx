bb057ad2e63662ddb2d006e1bffb0cb9c4a0e185
aldencheung2 <aldencheung2@gmail.com>
2022-10-01 20:42:15 -0400

    Added css

--- components/SearchInput/SearchInput.jsx
+++ components/SearchInput/SearchInput.jsx
@@ -6,7 +6,14 @@ const SearchInput = () => {
             <Heading my={4}>
                 Search Person
             </Heading>
-            <Input placeholder='Basic usage' width="6xl" height={16}/>
+            {/* <Input placeholder='Search' width="3xl" height={16}/> */}
+            <div style={{width: '3xl', height: '16', display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <input id="searchQueryInput" type="text" name="searchQueryInput" placeholder="Search" value="" style={{width: '100%', height: '2.8rem', background: '#f5f5f5', outline: 'none', border: 'none', borderRadius: '1.625rem', fontSize: '1rem', padding: '0 3.5rem 0 1.5rem'}}/>
+                <button id="searchQuerySubmit" type="submit" name="searchQuerySubmit" style={{width: '3.5rem', height: '2.8rem', marginLeft: '-3.5rem', background: 'none', border: 'none', outline: 'none'}}>
+                <svg style={{width:'24px', height:'24px', viewBox:"0 0 24 24"}}><path fill="#666666" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+                </svg>
+                </button>
+            </div>
         </Flex>
         
     )
